445918c55c7f41a9d8e72d4856c2462fd77c2f1d
osamagucCitySoc <osama.cs.eng@gmail.com>
2014-08-27 10:18:54 +0400

    1- Added constant file. It stores all macros to be used across the app.

---
+++ FtcTask/Constants.h
@@ -0,0 +1,17 @@
+//
+//  Constants.h
+//  FtcTask
+//
+//  Created by OsamaMac on 8/27/14.
+//  Copyright (c) 2014 Osama Rabie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Constants : NSObject
+
+
+#define tableViewCellIdentifier = "tableViewCellIdentifier"
+#define flickrAPI = "d4a9ac4b1dfe979055e2ffb2e87dfcbf"
+
+@end
---
+++ FtcTask/Constants.m
@@ -0,0 +1,13 @@
+//
+//  Constants.m
+//  FtcTask
+//
+//  Created by OsamaMac on 8/27/14.
+//  Copyright (c) 2014 Osama Rabie. All rights reserved.
+//
+
+#import "Constants.h"
+
+@implementation Constants
+
+@end
